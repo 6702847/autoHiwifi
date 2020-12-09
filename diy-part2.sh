3e5f51fe63752eb38a1b061ce261f002ca6a51d2
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2020-12-09 13:02:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,3 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 git clone https://github.com/syncthing/syncthing.git package/lean/syncthing
-git clone https://github.com/openwrt/luci/tree/openwrt-19.07/applications/luci-app-watchcat.git package/lean/luci-app-watchcat
-git clone https://openwrt.org/packages/pkgdata/watchcat.git package/lean/watchcat

2b6d28303774071f3cc188fe93a7752673caa367
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2021-06-22 15:37:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+# Update date
+cur_date="`date +%Y-%m-%d`"
+ sed -i 's/R21/[UPDATE $cur_date ]R21/g' package/lean/default-settings
 
 git clone https://github.com/fw876/helloworld
 # git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus

27eaef8e7e21d43c0b5c9d4da6c4c73a6dbe09d8
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2021-06-23 10:40:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,6 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 # Update date
 cur_date="`date +%Y-%m-%d`"
- sed -i 's/R21/[UPDATE $cur_date ]R21/g' package/lean/default-settings
-
+sed -i "s/R21[.0-9]/R$cur_date ]R21/g" package/lean/default-settings
 git clone https://github.com/fw876/helloworld
-# git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
-# git clone https://github.com/syncthing/syncthing.git package/lean/syncthing
+

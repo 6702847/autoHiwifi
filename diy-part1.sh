d9d33dbbc4765e989918dc50cc263857a979ecde
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2022-05-06 15:50:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,8 +27,8 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> fee
 # git clone https://github.com/linkease/istore
 git clone https://github.com/openwrt/luci
 
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/adguardhome package/
-git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/
+git clone https://github.com/kenzok8/openwrt-packages/tree/master/adguardhome package/adguardhome
+git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
 
 
 

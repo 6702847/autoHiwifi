72cd4967340add4a99092b71a33948de0dbe8f25
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2022-05-07 10:10:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,8 +22,10 @@ echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.
 echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
 echo "src-git appfilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
-echo "src-git pushbot  https://github.com/zzsj0928/luci-app-pushbot.git" >> feeds.conf.default
-echo "src-git luci-app-adguardhome  https://github.com/rufengsuixing/luci-app-adguardhome.git" >> feeds.conf.default
+echo "src-git pushbot  https://github.com/tty228/luci-app-serverchan.git" >> feeds.conf.default
+echo "src-git  luci-app-wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon.git" >> feeds.conf.default
+
+
 
 
 

6aa449b7e969256fe1244b113d4706181a5ace32
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2022-05-07 12:57:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,16 +18,14 @@
 # rm -rf  package/lean/luci-theme-argon 
 # git clone  https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
 
+
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
-echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
-echo "src-git appfilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' >>feeds.conf.default
 echo "src-git pushbot  https://github.com/tty228/luci-app-serverchan.git" >> feeds.conf.default
 echo "src-git  wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon.git" >> feeds.conf.default
-
-
-svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome   
 
 
 

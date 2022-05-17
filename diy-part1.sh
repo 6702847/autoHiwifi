812ab289f26dca03587e8f4e055a9e986c3781b6
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2022-05-17 12:21:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,13 +24,32 @@
 #sed -i "/openclash/d" "feeds.conf.default"
 #sed -i "/pushbot/d" "feeds.conf.default"
 #sed -i "/smartdns/d" "feeds.conf.default"
+# fix netdata
+rm -rf ./feeds/packages/admin/netdata
+svn co https://github.com/DHDAXCW/packages/branches/ok/admin/netdata ./feeds/packages/admin/netdata
 
-#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
 echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
+
+
+
+
+
+
+# Add luci-app-smartdns & smartdns
 rm -rf packages/net/smartdns
 svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
+# Add Pandownload
+pushd package/lean
+svn co https://github.com/immortalwrt/packages/trunk/net/pandownload-fake-server
+popd
 
 
 

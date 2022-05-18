df82e80d3e91d2479ea94c6d56ba2e3ad2ad34c0
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2022-05-18 13:21:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,38 +18,19 @@
 # rm -rf  package/lean/luci-theme-argon 
 # git clone  https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
 
-
 #sed -i "/helloworld/d" "feeds.conf.default"
 #sed -i "/passwall/d" "feeds.conf.default"
 #sed -i "/openclash/d" "feeds.conf.default"
 #sed -i "/pushbot/d" "feeds.conf.default"
 #sed -i "/smartdns/d" "feeds.conf.default"
-# fix netdata
-rm -rf ./feeds/packages/admin/netdata
-svn co https://github.com/DHDAXCW/packages/branches/ok/admin/netdata ./feeds/packages/admin/netdata
-
 echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
 
-
-
-
-
-
-# Add luci-app-smartdns & smartdns
-rm -rf packages/net/smartdns
-svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
-svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
-
 # Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall packages/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2 packages/openwrt-passwall2
+git clone https://github.com/vernesong/OpenClash packages/OpenClash
 
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
-# Add Pandownload
-pushd package/lean
-svn co https://github.com/immortalwrt/packages/trunk/net/pandownload-fake-server
-popd
 
 
 

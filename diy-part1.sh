a9de01798c0a6aae0684ecf5cad94ddf63fbb2c4
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2022-06-13 09:31:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,29 +11,27 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 # add helloworld
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # theme argon
 # rm -rf  package/lean/luci-theme-argon 
 # git clone  https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
 
-#sed -i "/helloworld/d" "feeds.conf.default"
-#sed -i "/passwall/d" "feeds.conf.default"
-#sed -i "/openclash/d" "feeds.conf.default"
-#sed -i "/pushbot/d" "feeds.conf.default"
-#sed -i "/smartdns/d" "feeds.conf.default"
 
-#git clone https://github.com/kenzok8/small-package package/small-package
-
+# aoo filter
 # echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
-
 
 # Add luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 git clone https://github.com/vernesong/OpenClash package/OpenClash
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns   package/luci-app-smartdns
+
+# Kernel版本控制
+# sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
 
 
 
